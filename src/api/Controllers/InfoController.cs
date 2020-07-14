2243a28b4d511c4566fe0bafef6c1f60a850d658
Jochen van Wylick <jowylick@microsoft.com>
2020-07-14 16:15:05 +0200

    Added .NET Core API the resolves public IP

---
+++ src/api/Controllers/InfoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using Microsoft.ApplicationInsights;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfoController : ControllerBase
+    {
+        private readonly ILogger<InfoController> _logger;
+        private readonly TelemetryClient telemetry;
+
+        public InfoController(ILogger<InfoController> logger, TelemetryClient telemetryClient)
+        {
+            _logger = logger;
+            this.telemetry = telemetryClient;
+        }
+
+        [HttpGet]
+        public async Task<InfoResponse> Get()
+        {
+            var start = DateTime.Now;
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.ipify.org/");
+            var response = await client.GetAsync("/");
+            var myIp = await response.Content.ReadAsStringAsync();
+
+            this.telemetry.TrackDependency("api.ipify.org", "/", "Get public IP address", start, DateTime.Now.Subtract(start), response.IsSuccessStatusCode);
+            return new InfoResponse()
+            {
+                PublicIp = myIp
+            };
+        }
+    }
+}
---
+++ src/api/Controllers/InfoResponse.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace api
+{
+    public class InfoResponse
+    {
+        public string PublicIp { get; set; }
+    }
+}
